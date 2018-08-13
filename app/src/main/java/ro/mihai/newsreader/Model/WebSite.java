eeb2ad67af111db6d955fff13eeb86952c4799ee
micos7 <micos7@gmail.com>
2018-08-13 21:43:51 +0300

    Initial commit

---
+++ app/src/main/java/ro/mihai/newsreader/Model/WebSite.java
@@ -0,0 +1,32 @@
+package ro.mihai.newsreader.Model;
+
+import java.util.List;
+
+public class WebSite {
+    public String status;
+    private List<Source> sources;
+
+    public WebSite() {
+    }
+
+    public WebSite(String status, List<Source> sources) {
+        this.status = status;
+        this.sources = sources;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
+}
