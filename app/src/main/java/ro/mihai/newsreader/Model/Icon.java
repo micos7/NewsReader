71f256c1f67c574cd540611e8110ade533cdc187
micos7 <micos7@gmail.com>
2018-08-16 08:51:58 +0300

    Models

---
+++ app/src/main/java/ro/mihai/newsreader/Model/Icon.java
@@ -0,0 +1,8 @@
+package ro.mihai.newsreader.Model;
+
+public class Icon {
+    private String url;
+    private int width,height,bytes;
+    private String format,sha1sum;
+    private Object error;
+}
---
+++ app/src/main/java/ro/mihai/newsreader/Model/IconBetterIdea.java
@@ -0,0 +1,10 @@
+package ro.mihai.newsreader.Model;
+
+import android.graphics.drawable.Icon;
+
+import java.util.List;
+
+public class IconBetterIdea {
+    private String url;
+    private List<Icon> icons;
+}
