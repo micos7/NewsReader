b0855a481cf897357a89a502c280a35d4155c382
micos7 <micos7@gmail.com>
2018-08-17 08:04:30 +0300

    Icons getter setter

--- app/src/main/java/ro/mihai/newsreader/Model/IconBetterIdea.java
+++ app/src/main/java/ro/mihai/newsreader/Model/IconBetterIdea.java
@@ -1,10 +1,24 @@
 package ro.mihai.newsreader.Model;
 
-import android.graphics.drawable.Icon;
-
 import java.util.List;
 
 public class IconBetterIdea {
     private String url;
     private List<Icon> icons;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<Icon> getIcons() {
+        return icons;
+    }
+
+    public void setIcons(List<Icon> icons) {
+        this.icons = icons;
+    }
 }
