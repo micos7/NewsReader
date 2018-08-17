320c86189f81cfe2ad54d4ad7a901d67c8b2d257
micos7 <micos7@gmail.com>
2018-08-17 08:03:49 +0300

    Pass data intent

--- app/src/main/java/ro/mihai/newsreader/Adapter/ListSourceAdapter.java
+++ app/src/main/java/ro/mihai/newsreader/Adapter/ListSourceAdapter.java
@@ -1,6 +1,7 @@
 package ro.mihai.newsreader.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,8 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import ro.mihai.newsreader.Common.Common;
+import ro.mihai.newsreader.Interface.IconBetterIdeaService;
 import ro.mihai.newsreader.Interface.ItemClickListener;
+import ro.mihai.newsreader.ListNews;
+import ro.mihai.newsreader.Model.IconBetterIdea;
 import ro.mihai.newsreader.Model.WebSite;
 import ro.mihai.newsreader.R;
 
@@ -24,6 +34,8 @@ class ListSourceViewHolder extends RecyclerView.ViewHolder implements View.OnCli
 
         source_image = itemView.findViewById(R.id.source_image);
         source_title = itemView.findViewById(R.id.source_title);
+
+        itemView.setOnClickListener(this);
     }
 
     public void setItemClickListener(ItemClickListener itemClickListener) {
@@ -39,11 +51,15 @@ class ListSourceViewHolder extends RecyclerView.ViewHolder implements View.OnCli
 public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder> {
     private Context context;
     private WebSite webSite;
+    private IconBetterIdeaService mService;
 
     public ListSourceAdapter(Context context, WebSite webSite) {
         this.context = context;
         this.webSite = webSite;
+
+        mService = Common.getIconService();
     }
+
 
     @NonNull
     @Override
@@ -54,8 +70,38 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ListSourceViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ListSourceViewHolder holder, int position) {
+        StringBuilder iconBetterApi = new StringBuilder("https://icons.better-idea.org/allicons.json?url=");
+        iconBetterApi.append(webSite.getSources().get(position).getUrl());
 
+        mService.getIconUrl(iconBetterApi.toString())
+                .enqueue(new Callback<IconBetterIdea>() {
+                    @Override
+                    public void onResponse(Call<IconBetterIdea> call, Response<IconBetterIdea> response) {
+                        if(response.body().getIcons().size() > 0){
+                            Picasso.get()
+                                    .load(response.body().getIcons().get(0).getUrl())
+                                    .into(holder.source_image);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<IconBetterIdea> call, Throwable t) {
+
+                    }
+                });
+
+        holder.source_title.setText(webSite.getSources().get(position).getName());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void OnClick(View view, int position, boolean isLongClick) {
+                Intent intent = new Intent(context, ListNews.class);
+                intent.putExtra("source",webSite.getSources().get(position).getId());
+                intent.putExtra("sortBy",webSite.getSources().get(position).getSortBysAvailable().get(0));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
