ee152e8341476c98b62f34634bb9fda31d7ea0e4
micos7 <micos7@gmail.com>
2018-08-18 10:57:58 +0300

    Article news models

---
+++ app/src/main/java/ro/mihai/newsreader/Model/Article.java
@@ -0,0 +1,53 @@
+package ro.mihai.newsreader.Model;
+
+public class Article {
+    private String author,title,description,url,urlToImage,PublishedAt;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return PublishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        PublishedAt = publishedAt;
+    }
+}
