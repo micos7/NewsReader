b79ec7df8623911a3bee49cb40479fe7a95f791a
micos7 <micos7@gmail.com>
2018-08-19 11:11:24 +0300

    Date missing

--- app/src/main/java/ro/mihai/newsreader/Adapter/ListNewsAdapter.java
+++ app/src/main/java/ro/mihai/newsreader/Adapter/ListNewsAdapter.java
@@ -1,6 +1,7 @@
 package ro.mihai.newsreader.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,9 +12,13 @@ import android.widget.TextView;
 import com.github.curioustechizen.ago.RelativeTimeTextView;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import ro.mihai.newsreader.Common.ISO8601Parse;
+import ro.mihai.newsreader.DetailArticle;
 import ro.mihai.newsreader.Interface.ItemClickListener;
 import ro.mihai.newsreader.Model.Article;
 import ro.mihai.newsreader.R;
@@ -67,10 +72,33 @@ public class ListNewsAdapter extends RecyclerView.Adapter<ListNewsViewHolder> {
         Picasso.get()
                 .load(articleList.get(position).getUrlToImage())
                 .into(holder.article_image);
-        if(articleList.get(position).getTitle().length()> 0){
+        if(articleList.get(position).getTitle().length()> 65){
             holder.article_title.setText(articleList.get(position).getTitle().substring(0,65)+ "...");
         }else{
             holder.article_title.setText(articleList.get(position).getTitle());
+
+            Date date = null;
+
+//            try {
+//                date = ISO8601Parse.parse(articleList.get(position).getPublishedAt());
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                holder.article_time.setReferenceTime(date.getTime());
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+
+
+            holder.setItemClickListener(new ItemClickListener() {
+                @Override
+                public void OnClick(View view, int position, boolean isLongClick) {
+                    Intent detail = new Intent(context,DetailArticle.class);
+                    detail.putExtra("webURL", articleList.get(position).getUrl());
+                    context.startActivity(detail);
+                }
+            });
         }
     }
 
